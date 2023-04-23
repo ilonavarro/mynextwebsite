dc0c0a455d1a76d28bc0678d4bd756361b8b54ba
ilonavarro <ilo.navarro+github@gmail.com>
2023-04-23 17:45:37 +0100

    Tests

--- src/components/Molecules/MMe/index.js
+++ src/components/Molecules/MMe/index.js
@@ -1,6 +1,9 @@
+import AImg from '@/components/Atoms/AImg'
+
 export default function MMe() {
   return (
     <>
+      <AImg />
       <h1>Olá, meu nome é Ilo Navarro...</h1>
       <h2>Ao longo da minha carreira eu...</h2>
       <p>E também fiz....</p>
